83c894de43ce56aa1236e444dd4514d697d90bfb
Unknown <icwei9@gmail.com>
2018-05-28 09:55:22 +1000

    Add in basic view layer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Redux from 'redux';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,13 +9,36 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Redux Example Todo App</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <AddTodo />
+          <VisibleTodoList />
         </p>
       </div>
     );
+  }
+}
+
+class AddTodo extends Component {
+  render() {
+    return (
+      <div>
+        <button>
+          Add Todo
+        </button>
+      </div>
+    )
+  }
+}
+
+class VisibleTodoList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>test item</li>
+      </ul>
+    )
   }
 }
 
