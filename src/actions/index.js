09d1820ee25d499c4d2b0af3da5bc889ef53ed41
Unknown <icwei9@gmail.com>
2018-05-29 12:45:15 +1000

    clean up on action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,33 +1,34 @@
-/*
- * action types
- */
-​
+// /*
+//  * action types
+//  */
+// ​
 export const ADD_TODO = 'ADD_TODO'
-export const TOGGLE_TODO = 'TOGGLE_TODO'
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
-​
-/*
- * other constants
- */
-​
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
+// export const TOGGLE_TODO = 'TOGGLE_TODO'
+// export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+// ​
+// /*
+//  * other constants
+//  */
+// ​
+// export const VisibilityFilters = {
+//   SHOW_ALL: 'SHOW_ALL',
+//   SHOW_COMPLETED: 'SHOW_COMPLETED',
+//   SHOW_ACTIVE: 'SHOW_ACTIVE'
+// }
 
-/*
- * action creators
- */
-​
+// /*
+//  * action creators
+//  */
+// ​
+let nextTodoId = 0
 export function addTodo(text) {
-  return { type: ADD_TODO, text }
+  return { id: nextTodoId++, type: ADD_TODO, text }
 }
-​
-export function toggleTodo(id) {
-  return { type: TOGGLE_TODO, id }
-}
-​
-export function setVisibilityFilter(filter) {
-  return { type: SET_VISIBILITY_FILTER, filter }
-}
+// ​
+// export function toggleTodo(id) {
+//   return { type: TOGGLE_TODO, id }
+// }
+// ​
+// export function setVisibilityFilter(filter) {
+//   return { type: SET_VISIBILITY_FILTER, filter }
+// }
