aed840df1b7ddeb9cca5660902733c7b962616e2
Unknown <icwei9@gmail.com>
2018-06-05 09:22:22 +1000

    make the code a little more readable

--- src/App.js
+++ src/App.js
@@ -1,66 +1,102 @@
-import React, { Component } from 'react';
-import Redux, { createStore } from 'redux';
+// Vendor Dependencies
+  import React, { Component } from 'react';
+  import { createStore } from 'redux';
+  import { Provider } from 'react-redux'
 
-import todoApp from './reducers/todos';
-import { addTodo } from './actions';
-
-import logo from './logo.svg';
-import './App.css';
-
-const initialState = {
-  todos: []
-}
+// Custome Dependencies
+  import AddTodo from './components/AddTodo'
+  import todoApp from './reducers/todos';
+  import { addTodo } from './actions';
+  import logo from './logo.svg';
+  import './App.css';
 
 const store = createStore(todoApp)
 
 // --- Testing store with reducer & action ---
-const unsubscribe = store.subscribe(() =>
-  console.log(store.getState())
-)
+  // const unsubscribe = store.subscribe(() =>
+  //   console.log(store.getState())
+  // )
 
-store.dispatch(addTodo('Learn about actions'))
-store.dispatch(addTodo('Learn about reducers'))
-store.dispatch(addTodo('Learn about store'))
+  // store.dispatch(addTodo('Learn about actions'))
+  // store.dispatch(addTodo('Learn about reducers'))
+  // store.dispatch(addTodo('Learn about store'))
 
-unsubscribe()
+  // unsubscribe()
 // --------------------------------------------
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Redux Example Todo App</h1>
-        </header>
-        <p className="App-intro">
+      <Provider store={store}>
+        <div className="App">
+          <AppHeader />
           <AddTodo />
-          <TodoList />
-        </p>
-      </div>
+          <TodoList todos={store.getState()}/>
+        </div>
+      </Provider>
     );
   }
 }
 
-class AddTodo extends Component {
+
+// 
+//  Components
+//  
+class AppHeader extends Component {
   render() {
     return (
-      <div>
-        <button onClick = {() => {
-          
-        }}>
-          Add Todo
-        </button>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">React Redux Example Todo App</h1>
+      </header>
     )
   }
 }
+
+
+// class AddTodo extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: '',
+//     };
+
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleChange(event) {
+//     this.setState({value: event.target.value});
+//   }
+//   handleSubmit(event) {
+//     store.dispatch(addTodo(this.state.value));
+//     this.setState({value: ''});
+//     console.log(store.getState())
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <form onSubmit={this.handleSubmit}>
+//           <input type="text" value={this.state.value} onChange={this.handleChange}/>
+//           <button type='submit'>
+//             Add Todo
+//           </button>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
 
 class TodoList extends Component {
   render() {
     return (
       <ul>
-        <li>test item</li>
+        {this.props.todos.map(todo => 
+          <li key={todo.id}>
+            {todo.text}
+          </li> )}
       </ul>
     )
   }
