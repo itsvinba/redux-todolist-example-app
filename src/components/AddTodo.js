6296f72ad6a02c78cf55ad15ee0fe362740b99cc
Unknown <icwei9@gmail.com>
2018-06-05 09:23:21 +1000

    AddTodo with connect function!

---
+++ src/components/AddTodo.js
@@ -0,0 +1,62 @@
+// Vendor Dependencies
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// Custom Dependencies
+import { addTodo } from '../actions';
+
+let value
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: () => dispatch(addTodo(value))
+  }
+}
+
+class AddTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    value = this.state.value;
+    this.props.addTodo();
+    console.log(this.props);
+    this.setState({value: ''});
+    // console.log(store.getState())
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          <button type='submit'>
+            Add Todo
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+) (AddTodo)
