bde3afcea32efae5fdcfcdcb0956024aad462de4
Unknown <icwei9@gmail.com>
2018-06-06 12:20:58 +1000

    Included Redux DevTool

--- src/App.js
+++ src/App.js
@@ -10,26 +10,33 @@
   import logo from './logo.svg';
   import './App.css';
 
-const store = createStore(todoApp)
+const store = createStore(
+  todoApp, 
+  /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+console.log(store.getState())
 
 // --- Testing store with reducer & action ---
-  const unsubscribe = store.subscribe(() =>
-    console.log(store.getState())
-  )
+  // const unsubscribe = store.subscribe(() =>
+  //   console.log(store.getState())
+  // )
 
   // store.dispatch(addTodo('Learn about actions'))
   // store.dispatch(addTodo('Learn about reducers'))
   // store.dispatch(addTodo('Learn about store'))
 
-  unsubscribe()
+  // unsubscribe()
 // --------------------------------------------
 
 class App extends Component {
+  
   render() {
     return (
       <Provider store={store}>
         <div className="App">
           <AppHeader />
+          <br/><br/>
           <AddTodo />
           <TodoList todos={store.getState()}/>
         </div>
